0216c625d55a5ba4071700743ae15c99ea5220c2
Omer Priel <omer2079@gmail.com>
2022-06-13 14:50:27 +0300

    complit

--- childs_draw/src/childs_draw.py
+++ childs_draw/src/childs_draw.py
@@ -6,7 +6,10 @@
 # Imports
 import os
 import sys
+import shutil
+
 import time
+import math
 
 import numpy as np
 import pandas as pd
@@ -15,6 +18,28 @@ import matplotlib.pyplot as plt
 
 import cv2
 
+import os
+import sys
+import shutil
+
+import time
+import math
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import cv2
+
+import src.childs_draw as childs_draw
+
+# Flags
+DELTA_XY = 40
+DIRECT_COUNT_RATIO = 10
+MAX_DIFF = 10
+
+# Configs
 data_root_folder = './data'
 save_images_folder = '.'
 
@@ -30,7 +55,9 @@ def set_data_root_folder(folder):
 def set_save_images_folder(folder):
     global save_images_folder
     save_images_folder = folder
-    
+
+
+# Loaders
 def load_image_data(folder_id: int, image_id: int):
     file_name = data_root_folder + '/' + str(folder_id) + '/SimpleTest/' + str(image_id)
     
@@ -48,69 +75,13 @@ def load_image_data(folder_id: int, image_id: int):
     
     return (img, df)
 
-def save_image(img):
-    global g_save_images_enabled
-    if not g_save_images_enabled:
-        return True
-    
-    global save_image_counter
-    global image_show_number_size
-    global save_images_folder
-    
-    save_image_counter += 1
-    number_as_str = str(save_image_counter)
-    while len(number_as_str) < image_show_number_size:
-        number_as_str = '0' + number_as_str
-    file_name = save_images_folder + '/show_' + number_as_str + '.png'
-    
-    if os.path.exists(file_name):
-        os.remove(file_name)
-    
-    return cv2.imwrite(file_name, img)
-
-def save_trace_image(img):
-    global g_save_images_enabled
-    if not g_save_images_enabled:
-        return True
-    
-    rgb_img = np.ndarray((img.shape[0], img.shape[1], 3))
-    
-    rgb_img[:, :, :] = 0
-    
-    found = np.where(img == 1)
-    for i in range(len(found[0])):
-        rgb_img[found[0][i],found[1][i], 0] = 255
-
-    found = np.where(img == 2)
-    for i in range(len(found[0])):
-        rgb_img[found[0][i],found[1][i], 2] = 255
-    
-    rgb_img[(rgb_img[:, :, 0] == 0) & (rgb_img[:, :, 1] == 0) & (rgb_img[:, :, 2] == 0)] = 255
-        
-    return save_image(rgb_img)
-
-
-def child_draw_describe(folder_id: int, image_id: int, save_images_enabled: bool = False):
-    global g_save_images_enabled
-    g_save_images_enabled = save_images_enabled
-    
+def load_draw(folder_id: int, image_id: int):
     (img, df) = load_image_data(folder_id, image_id)
     
     if img is None or df is None:
         return None
     
-    results = {
-        'Hands Up': None,
-        'Lines Count': None,
-        'Exist Closed Shapes': None,
-        'Closed Shapes Count': None
-    }
-    
     df.columns = map(lambda x: x.replace(" ", ""), df.columns.tolist())
-    
-    # Maybe not needable
-    last_message1 = df.iloc[-2]['X']
-    last_message2 = df.iloc[-1]['X']
     
     df = df.iloc[:-2].copy()
     
@@ -159,14 +130,194 @@ def child_draw_describe(folder_id: int, image_id: int, save_images_enabled: bool
     add_column_diff('TiltX')
     add_column_diff('TiltY')
     
-    # collect 'Hands Up'
-    results['Hands Up'] = (df.TimeDiff > df.TimeDiff.std()).sum()
+    return (img, df)
+
+
+# Lines Info
+def get_distance(x1, y1, x2, y2):
+    return math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
+
+def get_next_block(sub_df: pd.DataFrame, current, distance: float):
+    if current not in sub_df.index.tolist():
+        return None
+            
+    x1, y1 = sub_df.iloc[current][['X', 'Y']]
     
-    # collect 'Lines Count'
+    for i in range(current+1, len(sub_df)):
+        x2, y2 = sub_df.iloc[i][['X', 'Y']]
+        
+        distance -= get_distance(x1, y1, x2, y2)
+        
+        x1, y1 = x2, y2
+        
+        if distance <= 0:
+            return i
+    return len(sub_df) - 1
+
+def get_lines_in_sub_draw(sub_df: pd.DataFrame):
+    lines_cons = [0]
+    last_con_index = 0
     
-    return df
+    cons_x, cons_y = [], []
+    con_direct_lines_x, con_direct_lines_y = [], []
     
-    # collect 'Exist Closed Shapes' and 'Closed Shapes Count'
+    # Step 1
+    start_index = 0
+
+    start_index = 0
+    ls = sub_df.index.tolist()
+        
+    # Step 2
+    while start_index < len(ls):
+        end_index = get_next_block(sub_df, start_index, DELTA_XY)
+                
+        if start_index < end_index:
+            x1, y1 = sub_df.iloc[ls[start_index]][['X', 'Y']]
+            x2, y2 = sub_df.iloc[ls[end_index]][['X', 'Y']]
+            
+            points = sub_df.iloc[ls[start_index:end_index]][['X', 'Y']]
+            
+            direct_line_size = DIRECT_COUNT_RATIO * len(points.index)
+            direct_line_x = np.linspace(x1, x2, num=direct_line_size).tolist()
+            direct_line_y = np.linspace(y1, y2, num=direct_line_size).tolist()
+            
+            max_diff = 0
+            con_x, con_y = x1, y1
+            for point_i in points.index:
+                x,y = points.loc[point_i][['X', 'Y']]
+                
+                min_diff_point = get_distance(x, y, x1, y1)
+                
+                for di in range(direct_line_size):
+                    
+                    dis = get_distance(x, y, direct_line_x[di], direct_line_y[di])
+                    if dis > 0:
+                        if min_diff_point == 0:
+                            min_diff_point = dis
+                        else:
+                            min_diff_point = min([min_diff_point, dis])
+                
+                max_diff = max([max_diff, min_diff_point])
+                if max_diff == min_diff_point:
+                    con_x, con_y = x, y
+                    con_index = point_i
+                    
+                
+            if max_diff > MAX_DIFF:
+                start_con_index = last_con_index
+                last_con_index = con_index
+                
+                start_index = last_con_index
+                
+                lines_cons += [last_con_index]
+            else:
+                start_index += 1
+        else:
+            start_index += 1
+            
+    lines_cons += [len(sub_df.index) - 1]
+
+    lines = []
+    
+    for i in range(len(lines_cons) - 1):
+        start_index, end_index = lines_cons[i], lines_cons[i + 1]
+        
+        line = sub_df.loc[start_index: end_index]
+        
+        distance = 0
+        
+        for j in range(start_index, end_index):
+            p1 = sub_df.loc[j]
+            p2 = sub_df.loc[j + 1]
+            distance += get_distance(p1['X'], p1['Y'], p2['X'], p2['Y'])
+    
+        lines += [{
+            'Distance': distance,
+            'Mean Pressure': line['Pressure'].mean(),
+            'X': line['X'],
+            'Y': line['Y']
+        }]
+        
+    return lines
+
+def get_lines_info(df: pd.DataFrame):
+    hands_up = []
+    lines = []
+
+    limits = [df.head(1).index[0], df.tail(1).index[0]] + df[df.TimeDiff > df.TimeDiff.std()].index.tolist()
+    limits = np.unique(list(sorted(limits))).tolist()
+
+    index = 0
+
+    for i in range(len(limits) - 1):
+        start = limits[i]
+        end = limits[i+1]
+
+        sub_df = df.iloc[start:end].copy()
+        sub_df.index = range(len(sub_df.index))
+
+        sub_df = sub_df.iloc[1:]
+        sub_df.index = range(len(sub_df.index))
+
+        hands_up += [(sub_df['X'], sub_df['Y'])]
+
+        lines += get_lines_in_sub_draw(sub_df)
+
+    lines = pd.DataFrame(lines)
+    
+    res = {
+        'Hands Up Count': len(hands_up),
+        'Lines Count': len(lines),
+        'Hands Up Info': hands_up,
+        'Lines Info': lines
+    }
+    
+    return res
+
+# Images Saver
+def save_image(img):
+    global g_save_images_enabled
+    if not g_save_images_enabled:
+        return True
+    
+    global save_image_counter
+    global image_show_number_size
+    global save_images_folder
+    
+    save_image_counter += 1
+    number_as_str = str(save_image_counter)
+    while len(number_as_str) < image_show_number_size:
+        number_as_str = '0' + number_as_str
+    file_name = save_images_folder + '/show_' + number_as_str + '.png'
+    
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    
+    return cv2.imwrite(file_name, img)
+
+def save_trace_image(img):
+    global g_save_images_enabled
+    if not g_save_images_enabled:
+        return True
+    
+    rgb_img = np.ndarray((img.shape[0], img.shape[1], 3))
+    
+    rgb_img[:, :, :] = 0
+    
+    found = np.where(img == 1)
+    for i in range(len(found[0])):
+        rgb_img[found[0][i],found[1][i], 0] = 255
+
+    found = np.where(img == 2)
+    for i in range(len(found[0])):
+        rgb_img[found[0][i],found[1][i], 2] = 255
+    
+    rgb_img[(rgb_img[:, :, 0] == 0) & (rgb_img[:, :, 1] == 0) & (rgb_img[:, :, 2] == 0)] = 255
+        
+    return save_image(rgb_img)
+
+# collect 'Exist Closed Shapes' and 'Closed Shapes Count'
+def get_shapes_count(img, df: pd.DataFrame):
     # get zoomed images
     drawed_img = img.copy()
 
@@ -222,7 +373,8 @@ def child_draw_describe(folder_id: int, image_id: int, save_images_enabled: bool
         
         steps = 0
         while len(points) > 0:
-            (x,y) = points.pop()
+            (x,y) = points[0]
+            points.remove(points[0])
             if img[x,y] == 0:                
                 img[x,y] = 2
                 for n in [0, 1, 2, 3, 5, 6, 7, 8]:
@@ -241,22 +393,76 @@ def child_draw_describe(folder_id: int, image_id: int, save_images_enabled: bool
     save_trace_image(trace_img)
 
     if (trace_img == 0).sum() == 0:
-        results['Exist Closed Shapes'] = False
-        results['Closed Shapes Count'] = 0
-    else:
-        results['Exist Closed Shapes'] = True
-        
-        closed_shapes_count = 0
-        while (trace_img == 0).sum() > 0:
-            closed_shapes_count += 1
+        return 0
+    
+    closed_shapes_count = 0
+    while (trace_img == 0).sum() > 0:
+        closed_shapes_count += 1
             
-            found = np.where(trace_img == 0)
-            points = list()
-            points.append((found[0][0], found[1][0]))
-            trace_img = red_spider(trace_img, points)
-            save_trace_image(trace_img)
-        
-        results['Closed Shapes Count'] = closed_shapes_count
+        found = np.where(trace_img == 0)
+        points = list()
+        points.append((found[0][0], found[1][0]))
+        trace_img = red_spider(trace_img, points)
+        save_trace_image(trace_img)
     
+    return closed_shapes_count
+
+def child_draw_describe(folder_id: int, image_id: int, save_images_enabled: bool = False):
+    global g_save_images_enabled
+    g_save_images_enabled = save_images_enabled
+   
+    results = {
+        'Hands Up Count': None,
+        'Lines Count': None,
+        'Exist Closed Shapes': None,
+        'Closed Shapes Count': None,
+        'Hands Up Info': None, # for ChildDrawDescribe
+        'Lines Info': None # for ChildDrawDescribe
+    }
+
+    img, df = load_draw(folder_id, image_id)
+
+    lines_info = get_lines_info(df)
+    closed_shapes_count = get_shapes_count(img, df)
+
+    results['Hands Up Count'] = lines_info['Hands Up Count']
+    results['Lines Count'] = lines_info['Lines Count']
+    results['Hands Up Info'] = lines_info['Hands Up Info']
+    results['Lines Info'] = lines_info['Lines Info']
+
+    results['Exist Closed Shapes'] = closed_shapes_count != 0
+    results['Closed Shapes Count'] = closed_shapes_count
+
     return results
+
+class ChildDrawDescribe:
+    def __init__(self, folder_id, image_id, save_images_enabled: bool = False):
+        # Get Data
+        self.results = child_draw_describe(folder_id, image_id, save_images_enabled)
     
+    def get_results(self):
+        return {
+            'Hands Up Count': self.results['Hands Up Count'],
+            'Lines Count': self.results['Lines Count'],
+            'Exist Closed Shapes': self.results['Exist Closed Shapes'],
+            'Closed Shapes Count': self.results['Closed Shapes Count'],
+            'Lines Info': self.results['Lines Info'][['Distance', 'Mean Pressure']]
+        }        
+    
+    def print_lines_plots(self):
+        hands_up = self.results['Hands Up Info']
+        lines = self.results['Lines Info']
+        
+        fig = plt.figure(figsize=(14, 7))
+        
+        ax = fig.add_subplot(1,2,1)
+        ax.set_title('Hands Up')
+        for points in hands_up:
+            plt.scatter(points[0], points[1])
+
+        ax = fig.add_subplot(1,2,2)
+        ax.set_title('Lines')
+        for line_index in lines.index:
+            plt.scatter(lines.loc[line_index]['X'], lines.loc[line_index]['Y'].tolist())
+        
+        plt.show()
